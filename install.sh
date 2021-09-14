073c8c4360c9a62e511c139bb56fb997585fe16b
Garrett <86270631+ArtistXoder@users.noreply.github.com>
2021-09-13 22:08:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo would you like to install Firefox and Kdenlive? 
+read -p "Continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) echo "yes";;
+  n|N ) echo "no";;
+  * ) echo "invalid";;
+esac
 sudo apt-get install kdenlive 
 sudo apt-get install firefox-esr
